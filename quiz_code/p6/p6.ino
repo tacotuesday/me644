b418f3116d6e39bea6de67b019f028a783b673c5
tacotuesday <grafton.cook@gmail.com>
2018-06-24 14:14:19 -0400

    Finished p6.

--- quiz_code/p6/p6.ino
+++ quiz_code/p6/p6.ino
@@ -1,76 +1,59 @@
 //p2.ino -- Practice for quiz 5, problem 2.
 
-const byte grButton = 13;
-const byte rButton = 12;
-const byte yButton = 11;
-const byte grLED = 5;
-const byte rLED = 4;
-const byte yLED = 3;
+const byte opSwitch = 13;
+const byte buttonOne = 12;
+const byte buttonTwo = 11;
+const byte ledPin = 5;
 
-int grCounter = 0;
-byte grState = 0;
-byte lastGrState = 0;
-int yCounter = 0;
-byte yState = 0;
-byte lastYState = 0;
-int rCounter = 0;
-byte rState = 0;
-byte lastRState = 0;
+int switchState = 0;
+byte oneState = 0;
+byte lastOneState = 0;
+int oneCounter = 0;
+int oneTotal = 0;
+byte twoState = 0;
+byte lastTwoState = 0;
+int twoCounter = 0;
+int twoTotal = 0;
+int grandTotal = 0;
 
 void setup() {
-  pinMode(grButton, INPUT);
-  pinMode(yButton, INPUT);
-  pinMode(rButton, INPUT);
-  pinMode(grLED, OUTPUT);
-  pinMode(rLED, OUTPUT);
-  pinMode(yLED, OUTPUT);
+  pinMode(opSwitch, INPUT);
+  pinMode(buttonTwo, INPUT);
+  pinMode(buttonOne, INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  grState = digitalRead(grButton);
-  rState = digitalRead(rButton);
-  yState = digitalRead(yButton);
-  if (grState != lastGrState) {
-    if (grState == HIGH) {
-      grCounter++;
-      Serial.print("number of green pushes: ");
-      Serial.println(grCounter);
+  switchState = digitalRead(opSwitch);
+  oneState = digitalRead(buttonOne);
+  twoState = digitalRead(buttonTwo);
+  if (oneState != lastOneState) {
+    if (oneState == HIGH) {
+      oneCounter++;
     }
+    oneTotal = oneCounter;
     delay(50);
   }
-  lastGrState = grState;
-  if (rState != lastRState) {
-    if (rState == HIGH) {
-      rCounter++;
-      Serial.print("number of red pushes: ");
-      Serial.println(rCounter);
+  lastOneState = oneState;
+  if (twoState != lastTwoState) {
+    if (twoState == HIGH) {
+      twoCounter++;
     }
+    twoTotal = twoCounter;
     delay(50);
   }
-  lastRState = rState;
-  if (yState != lastYState) {
-    if (yState == HIGH) {
-      yCounter++;
-      Serial.print("number of yellow pushes: ");
-      Serial.println(yCounter);
-    }
-    delay(50);
-  }
-  lastYState = yState;
-  if (grCounter > rCounter && grCounter > yCounter) {
-    digitalWrite(rLED, LOW);
-    digitalWrite(yLED, LOW);
-    digitalWrite(grLED, HIGH);
-   }
-  else if (rCounter > grCounter && rCounter > yCounter) {
-    digitalWrite(grLED, LOW);
-    digitalWrite(yLED, LOW);
-    digitalWrite(rLED, HIGH);
-  }
-   else if (yCounter > grCounter && yCounter > rCounter) {
-    digitalWrite(grLED, LOW);
-    digitalWrite(rLED, LOW);
-    digitalWrite(yLED, HIGH);
+  lastTwoState = twoState;
+  if (switchState > 0) { digitalWrite(ledPin, HIGH); }
+  switch (switchState) {
+    case 0:
+      grandTotal = 0;
+      grandTotal = oneTotal + twoTotal;
+      Serial.println(grandTotal);
+      break;
+    case 1:
+      grandTotal = 0;
+      grandTotal = oneTotal - twoTotal;
+      Serial.println(grandTotal);
+      break;
   }
 }
