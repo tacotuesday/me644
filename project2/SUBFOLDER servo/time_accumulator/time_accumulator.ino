ac753c912413b3afed53706fbe78fd3c5729ebbb
tacotuesday <grafton.cook@gmail.com>
2018-06-29 11:07:31 -0400

    Uploaded time_accumulator.ino
    time_accumulator now works as desired.

--- project2/SUBFOLDER servo/time_accumulator/time_accumulator.ino
+++ project2/SUBFOLDER servo/time_accumulator/time_accumulator.ino
@@ -1,25 +1,29 @@
 #include <Servo.h>
 
-// create servo object to control a servo
+// Create servo object
 Servo test_servo;
 
-// pin definition
+// Pin definition
 const byte servo_pin = 12;
 const byte speed_pin = 7;
-unsigned long measured_t_pass = 100;    // Use long for time-holding variables
+// Declare time-holding variables using long to avoid overflow
+unsigned long measured_t_pass = 100;
 unsigned long test_time = 0;
 
 // initial center values
-int center_value       = 2000;    // Set this to the lower or upper limit
+int center_value = 2000;  // Set this to the lower or upper limit
 
 void setup() {
   pinMode(speed_pin, INPUT_PULLUP);
   Serial.begin(9600);
-  while(measured_t_pass >= 20) {  // Experimentally-determined top speed
-    test_time = millis();
+  // Experimentally-determined t_pass at max motor speed = 20ms.
+  // This is for ONE specific servo: modify the below value based on calibration
+  // curve for the servo motor being evaluated.
+  while(measured_t_pass >= 20) {
     test_servo.attach(servo_pin);
+    test_time = millis(); // Start timing the servo
     test_servo.writeMicroseconds(center_value);
-    measured_t_pass = read_encoder();
+    measured_t_pass = read_encoder(); // Get time for one spoke pass
     Serial.print("Center Value = ");
     Serial.print(center_value);
     Serial.print(", ");
@@ -32,8 +36,8 @@ void setup() {
   }
 }
 
-void loop() {
-}
+void loop() { }
+
 
 long read_encoder() {
   unsigned long t_pass;
