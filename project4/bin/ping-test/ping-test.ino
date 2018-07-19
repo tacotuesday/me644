ca1ac70e1c696b32033f28e0b92fd50668a0ec1c
tacotuesday <grafton.cook@gmail.com>
2018-07-19 18:52:14 -0400

    Updated wall-seeking algorithm and encoder counter

--- project4/bin/ping-test/ping-test.ino
+++ project4/bin/ping-test/ping-test.ino
@@ -1,20 +1,3 @@
-/* Ping))) Sensor
-   This sketch reads a PING))) ultrasonic rangefinder and returns the
-   distance to the closest object in range. To do this, it sends a pulse
-   to the sensor to initiate a reading, then listens for a pulse
-   to return.  The length of the returning pulse is proportional to
-   the distance of the object from the sensor.
-
-   The circuit:
-   * +V connection of the PING))) attached to +5V
-   * GND connection of the PING))) attached to ground
-   * SIG connection of the PING))) attached to digital pin 8
-
-   http://www.arduino.cc/en/Tutorial/Ping
-   created 3 Nov 2008 by David A. Mellis
-   modified 30 Aug 2011 by Tom Igoe
-   This example code is in the public domain. */
-
 const int pingPin = 8;  // sensor signal pin
 // establish variables for ping duration distance (in., cm)
 unsigned long duration, inches, last_inches, display_inches, cm;
@@ -22,43 +5,46 @@ unsigned long duration, inches, last_inches, display_inches, cm;
 void setup() {
   Serial.begin(9600);   // initialize serial communication
 
-  inches = 72;
-  while (inches > 20) {
-  // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
-  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
-  pinMode(pingPin, OUTPUT);
-  digitalWrite(pingPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(pingPin, HIGH);
-  delayMicroseconds(5);
-  digitalWrite(pingPin, LOW);
-
-  // The same pin is used to read the signal from the PING))): a HIGH
-  // pulse whose duration is the time (in microseconds) from the sending
-  // of the ping to the reception of its echo off of an object.
-  pinMode(pingPin, INPUT);
-  duration = pulseIn(pingPin, HIGH);
-
-  // convert the time into a distance
-  inches = microsecondsToInches(duration);
-  cm     = microsecondsToCentimeters(duration);
-
-  Serial.print(inches);
-  Serial.print(" in,    ");
-  Serial.print(cm);
-  Serial.print(" cm");
-  Serial.println();
-
-  delay(100);
-  last_inches = inches;
-  display_inches = last_inches + 1000;
-  }
+  test666 = find_wall();
   Serial.print("The last recorded value was ");
-  Serial.println(display_inches);
+  Serial.println(test_666);
 }
 
 void loop() {
-  
+
+}
+
+unsigned long find_wall() {
+  inches = 150;
+  while (inches > 110) {
+    // Trigger the PING))).
+    pinMode(pingPin, OUTPUT);
+    digitalWrite(pingPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(pingPin, HIGH);
+    delayMicroseconds(5);
+    digitalWrite(pingPin, LOW);
+
+    // Read the signal from the PING))): a HIGH pulse whose duration is the time
+    // (in microseconds) from the TX to RX echo off object.
+    pinMode(pingPin, INPUT);
+    duration = pulseIn(pingPin, HIGH);
+
+    // Time to distance conversion.
+    inches = microsecondsToInches(duration);
+    cm     = microsecondsToCentimeters(duration);
+
+    // Serial debugging for non-headless mode.
+    Serial.print(inches);
+    Serial.print(" in,    ");
+    Serial.print(cm);
+    Serial.print(" cm");
+    Serial.println();
+
+    delay(100);
+    last_inches = inches;
+    return last_inches;
+  }
 }
 
 unsigned long microsecondsToInches(unsigned long microseconds) {
