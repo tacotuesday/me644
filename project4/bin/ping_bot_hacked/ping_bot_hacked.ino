a84d4fbdd97e3801b2c9b5b60b2b7cf7c79537f3
tacotuesday <grafton.cook@gmail.com>
2018-07-22 21:34:37 -0400

    Reformatted production code

--- project4/bin/ping_bot_hacked/ping_bot_hacked.ino
+++ project4/bin/ping_bot_hacked/ping_bot_hacked.ino
@@ -13,8 +13,8 @@ Servo right_servo, left_servo, Ping_servo;
 
 // Pin Definition
 const byte   right_servo_pin = 12;
-const byte    left_servo_pin = 10;
 const byte right_encoder_pin = 11;
+const byte    left_servo_pin = 10;
 const byte  left_encoder_pin =  9;
 const byte    Ping_servo_pin =  8;
 const byte          Ping_pin =  7;
@@ -22,31 +22,26 @@ const byte          Ping_pin =  7;
 // servo center values
 int right_center_value = 1499;
 int  left_center_value = 1486;
+int Ping_right_value = 6;
 int Ping_center_value = 96;
 int Ping_left_value = 180;
-int Ping_right_value = 6;
-
 
 // initialize variables, counters, and desired travel distance
-// = desired distance ft * (12 in/ft * 64 encoder_changes/rotation / 8 in/rotation)
-byte runonce = 1;                // flag to run loop once
-int distance = 1*(12*64/8);      // = # of 0.125" w fine encoder wheels
-int half_dist = (1*(12*64/8))/2; // used to go forward six inches
+// feet traveled = desired distance ft * (12 in/ft * 64 encoder_changes/rotation / 8 in/rotation)
+byte runonce = 1;               // flag to run loop once
+int inches;                     // distance to wall
 long duration;
-unsigned long inches;            // distance to wall
-volatile int cc_left, cc_right;           // left encoder counter
-volatile int feet_traveled;
-volatile int current_dist;
+volatile int cc_left, cc_right;
 
 // PID variables & initialization
-double dt;                // time difference between encoders
-double desired_dt  = 0;   // desired time difference between encoders
+double dt;                      // time difference between encoders
+double desired_dt  = 0;         // desired time difference between encoders
 double    left_spd = 50;
 double   right_spd = 56;
 // PID myPID(&dt, &right_spd, &desired_dt, 0.084457214, 7.712170356, 0.014842, DIRECT);
 
 void setup() {
-  Serial.begin(115200);         // initialize USB communication
+  Serial.begin(115200);         // Initialize serial communication
    radio.begin();
    radio.setPALevel(RF24_PA_LOW);
 
@@ -68,34 +63,55 @@ void loop() {
   if (runonce == 1) {
     look(Ping_center_value);            // Orient the PING))) straight ahead.
     attach_servos(1);
-    delay(50);
+    delay(100);
     orient_encoders();
-    delay(50);
-    cc_left = 0; current_dist = 0;
-    look(Ping_left_value);    // Orient the PING))) 90 degrees to the left.
+    delay(100);
+    cc_left = 0;
+    look(Ping_right_value);    // Orient the PING))) 90 degrees to the left.
 
     cc_left = 0;
     int Ping_measure = 150; // Initialize a measured range to enter the while loop
     int max_range = 84;     // Max scan range ~= 7 ft
 
-    // Search for first wall
-    while (Ping_measure > max_range) {Ping_measure = distance_measure(); drive(left_spd, right_spd); }
+    // Search for first wall surrounding bridge
+    while (Ping_measure > max_range) { Ping_measure = distance_measure(); drive(left_spd, right_spd); }
+    delay(100); drive(0, 0); delay(100); orient_encoders(); delay(100);
+    n1 = cc_left / 8;           // n1 = y-coordinate distance to wall start -> cmd[0]
+    e1 = distance_measure();    // e1 = x-coordinate distance to wall -> cmd[1]
+    delay(100);
+    cc_left = 0; cc_right = 0;  // reset encoder counts
 
+    // Move forward 18 inches to align with bridge
+    while (cc_left < 144) { drive(left_spd, right_spd); }
+    delay(100); orient_encoders(); delay(100); cc_left = 0;
+    look(Ping_left_value); delay(100);    // orient the Ping toward expected treasure direction
 
-    if (!radio.write( &inches, sizeof(unsigned long) )) { Serial.println(F("failed")); }
-    // cc_left = 0;
-    // while (cc_left < 185) { drive(-65, -50); }
-    // drive(0, 0); delay(1000);
+    // Turn right to cross the bridge
+    while (cc_left < 19 || cc_right < 19) { drive(left_spd - 30, right_spd + 30); }
+    delay(100); drive(0, 0); delay(100); orient_encoders(); delay(100);
+    cc_left = 0; cc_right = 0;
+
+    // Cross the bridge to grabber bot's y-coordinate: e1 + 2'
+    while (cc_left < (e1 + 24) * (64 / 8)) { drive(left_spd, right_spd); }
+    delay(100); drive(0, 0); delay(100); orient_encoders(); delay(100);
+
+    // Move forward to find the treasure
+    while (Ping_measure > max_range) { Ping_measure = distance_measure(); drive(left_spd, right_spd); }
+    delay(100); drive(0, 0); delay(100); orient_encoders(); delay(100);
+    e2 = cc_left / 8;           // e2 = y-coordinate distance to wall start -> cmd[2]
+    n2 = distance_measure();    // n2 = x-coordinate distance to wall -> cmd[3]
+    delay(100);
+    cc_left = 0; cc_right = 0;
+
+    // Move out of grabber bot's path
+    while (cc_left < 24 * (64 / 8)) { drive(left_spd, right_spd); }
+    delay(100); drive(0, 0);
+
+    cmd[] ={n1, e1, e2, n2};
+
+    if (!radio.write( &cmd, sizeof(int) )) { Serial.println(F("failed")); }
     attach_servos(0); runonce = 0;
   }
-}
-
-void one_foot_forward() {
-  int distance = 1*(12*64/8);
-  while (cc_left < distance) {
-    drive(right_spd, left_spd);
-  }
-  drive(0, 0); delay(1000);
 }
 
 int distance_measure() {
@@ -105,30 +121,14 @@ int distance_measure() {
   digitalWrite(Ping_pin, HIGH);
   delayMicroseconds(5);
   digitalWrite(Ping_pin, LOW);
-
-  // The same pin is used to read the signal from the PING))): a HIGH
-  // pulse whose duration is the time (in microseconds) from the sending
-  // of the ping to the reception of its echo off of an object.
   pinMode(Ping_pin, INPUT);
   duration = pulseIn(Ping_pin, HIGH);
-
-  // convert the time into a distance
-  inches = duration / 67UL / 2UL;
+  inches = duration / 67UL / 2UL;        // Expanded to remove microsecondsToInches()
 
   Serial.print(inches);
   Serial.print(" in,    ");
-if (inches > 1) {return inches;}
+  if (inches > 1) {return inches;}
 }
-
-// unsigned long microsecondsToInches(unsigned long microseconds) {
-//   // According to Parallax's datasheet for the PING))), there are
-//   // 73.746 microseconds per inch (i.e. sound travels at 1130 feet per
-//   // second).  This gives the distance travelled by the ping, outbound
-//   // and return, so we divide by 2 to get the distance of the obstacle.
-//   // See: http://www.parallax.com/dl/docs/prod/acc/28015-PING-v1.3.pdf
-//   return microseconds / 67UL / 2UL;
-//   // 1 / ( 67 us/in * 1 s/1e6 us * 12 in / ft ) = 1243.8 ft/s = 379.1 m/s!
-// }
 
 void look(int Ping_servo_position) {
   Ping_servo.attach(Ping_servo_pin);
@@ -177,4 +177,5 @@ void drive(double right_speed, double left_speed) {
    left_servo.writeMicroseconds( left_center_value +  left_speed);
 }
 
+void  left_counter() { cc_left++; }
 void  left_counter() { cc_left++; }
